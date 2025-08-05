f5dff02985b376d6bb3f7f35a701e29aba9ab6f6
UlyanaGru <giperbolicheskiisinys@gmail.com>
2025-08-05 08:25:10 +0300

    func for generation comb 6 cards

--- cards.cxx
+++ cards.cxx
@@ -7,3 +7,20 @@ using namespace std;
 // Глобальные переменные для подсчета комбинаций
 int totalCombinations = 0;
 int successfulCombinations = 0;
+
+// Функция для генерации всех комбинаций 6 карт
+void generateCombinations(const vector<int>& cards, int start, int k, 
+                         vector<int>& current, int currentSum) {
+    if (k == 0) {
+        totalCombinations++;
+        if (currentSum == 21) {
+            successfulCombinations++;
+        }
+        return;
+    }
+    for (int i = start; i <= cards.size() - k; i++) {
+        current.push_back(cards[i]);
+        generateCombinations(cards, i + 1, k - 1, current, currentSum + cards[i]);
+        current.pop_back();
+    }
+}

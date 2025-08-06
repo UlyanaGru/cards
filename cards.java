9b2aad8284e09c72667e5514953a612adbeba4a4
UlyanaGru <giperbolicheskiisinys@gmail.com>
2025-08-06 08:16:43 +0300

    createed func to fill the list of cards

--- cards.java
+++ cards.java
@@ -1,2 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+
+public class CardProbability {
+    public static void main(String[] args) {
+        // Создаем список всех карт в колоде с их очками
+        List<Integer> cards = new ArrayList<>();
+        // Карты от 2 до 10, валет (11), дама (12), король (13), туз (14)
+        // В колоде 4 масти для каждого номинала, всего 52 карты
+        for (int value = 2; value <= 14; value++) {
+            int points = value; // Очки совпадают с номиналом для всех карт
+            for (int i = 0; i < 4; i++) { // 4 масти
+                cards.add(points);
+            }
+        }
